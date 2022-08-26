df65bfcc1e4676a8adf20a17999065b621776966
StarDiadka <vvvmitri@mail.ru>
2022-08-26 05:21:37 +0300

    новое

---
+++ python/распокавать_списки_внутри_списка.py
@@ -0,0 +1,9 @@
+#  распокавать_списки_внутри_списка
+def get_line_list(d,a=[]):
+    for i in d:
+        if  not isinstance(i, list):
+            a.append(i)
+        elif isinstance(i, list):
+            get_line_list(i, a)
+             
+    return a   
