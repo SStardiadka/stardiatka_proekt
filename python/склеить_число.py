6280fa506411357c26d74b0fe95af211ae3082d6
StarDiadka <vvvmitri@mail.ru>
2022-09-03 04:06:29 +0300

    1

--- python/склеить_число.py
+++ python/склеить_число.py
@@ -1,5 +1,5 @@
-#  склеить_число
 def ckl_chislo(lst):
+    'СКЛЕИТЬ ЧИСЛО'
     if len(lst)== 0:
         return -1
     else:
@@ -7,3 +7,4 @@ def ckl_chislo(lst):
         chislo = ''.join(sorted(a, key =lambda z: tuple(z + z*100), reverse= True))
         return int(chislo)
 print(ckl_chislo([7, 71, 72]))
+print(ckl_chislo.__doc__)
