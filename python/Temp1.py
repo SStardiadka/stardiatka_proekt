c3006d8356560b6cdc2ab72c0ebe0d0c87bcf0bd
StarDiadka <vvvmitri@mail.ru>
2022-12-17 20:37:13 +0300

    17.12.22

--- python/Temp1.py
+++ python/Temp1.py
@@ -1,19 +1,4 @@
-class Cat:
-   
-
-    def __init__(self, name={}, age=None, isHappy=None):
-        self.name = name
-        self.age = age
-        self.isHappy = isHappy
-        self.get_data()
-
-   
-    def get_data(self):
-        print(self.name, "age:", self.age, ". Happy:", self.isHappy)
-
-
-cat1 = Cat()
-
-
-cat2 = Cat("Жопен", 2, False)
-print(cat2.name)
+a,b,c,d=int(input()),int(input()),int(input()),int(input()) 
+print(" ")  
+for i in range(c,d+1): 
+    print("\t",i,end="")
