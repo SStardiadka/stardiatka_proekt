b6498f41325ed77312bafa155bfee8cc6571ac9e
StarDiadka <vvvmitri@mail.ru>
2022-12-25 12:39:35 +0300

    25.12.22

--- python/tablica.py
+++ python/tablica.py
@@ -1,8 +1,10 @@
 from prettytable import PrettyTable
 table = PrettyTable()
-table.field_names =(['name','age','citi'])
-table.add_row(['дима','48','дунаев'])
-table.add_row(['ало','7','дунаев'])
+table.field_names =(['name','age','citi', 'er'])
+table.add_row(['дима','48','дунаев','1'])
+table.add_row(['ало','7','дунаев', '2'])
 table.align = 'c'
 table.sortby = 'age'
 print(table)
+with open(r'C:\stardiatka_proekt-1\python\Rfiles\tabl.txt', 'w', encoding= 'utf-8"') as f:
+    print(table, file=f)
