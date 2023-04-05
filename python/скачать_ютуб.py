e14a4888d3678149c6b60eec09c6d8c89fb444cf
StarDiadka <vvvmitri@mail.ru>
2023-04-05 19:23:18 +0300

    05.04.23

--- python/скачать_ютуб.py
+++ python/скачать_ютуб.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 
 mylist = ["ok"]
 for i in tqdm(mylist):
-    yt = YouTube(r"https://youtu.be/B5GNie5w8lQ")
+    yt = YouTube(r"https://youtu.be/pFOBQQ6XcTM")
     ys = yt.streams.get_highest_resolution()
     ys.download(r"C:\stardiatka_proekt-1\python\Wfiles")
     print(i)
